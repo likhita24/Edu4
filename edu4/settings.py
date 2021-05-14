3132d4d0c4b7bb0a19ad0ad05592b7cffff280ad
Sri Likhita Bitra <53465719+likhita24@users.noreply.github.com>
2021-05-14 19:17:36 +0530

    Update settings.py

--- edu4/settings.py
+++ edu4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fap=1uw)q&4)tgk-==0e1m4hvhw&o_26esdhpe9d)ji)5c8e3$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['edu4test.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['edu4.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
