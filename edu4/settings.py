95ffa35e26d36c417c479800d880b96c4e8aee18
likhita24 <srilikitha.bitra@gmail.com>
2021-05-15 17:38:55 +0530

    TLS to SSL

--- edu4/settings.py
+++ edu4/settings.py
@@ -137,8 +137,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'serahliz.best@gmail.com'
 EMAIL_HOST_PASSWORD = '#btsot7magic'
 
